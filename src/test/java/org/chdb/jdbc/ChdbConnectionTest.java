e7a411fe0626ff0460147ac842b322ef7399afa8
Libing Chen <libing.chen@gmail.com>
2024-09-01 11:29:19 +0800

    test: make id as param

--- src/test/java/org/chdb/jdbc/ChdbConnectionTest.java
+++ src/test/java/org/chdb/jdbc/ChdbConnectionTest.java
@@ -1,6 +1,8 @@
 package org.chdb.jdbc;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AutoClose;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.sql.*;
 
@@ -27,9 +29,10 @@ public class ChdbConnectionTest {
 
     @Test
     public void testPreparedStatement() throws SQLException {
-        String sql = "select * from file('src/test/resources/logs.csv','CSV') where level = ? and id > 0";
+        String sql = "select * from file('src/test/resources/logs.csv','CSV') where level = ? and id > ?";
         ChdbPreparedStatement statement = (ChdbPreparedStatement) conn.prepareStatement(sql);
         statement.setString(1, "error");
+        statement.setInt(2, 0);
         System.out.println(statement.getRealSQL());
         ResultSet resultSet = statement.executeQuery();
         while (resultSet.next()) {
